2321ae92c496511dca78fb2fc760e4b412dfdb3d
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-05 17:22:36 +0530

    added signin callbacks

--- app/auth.ts
+++ app/auth.ts
@@ -1,13 +1,38 @@
 
+import { client } from "@/sanity/lib/client"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import { writeClient } from "@/sanity/lib/write-client"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
+    async signIn({
+      user:{name, email, image},
+      profile:{id,login ,bio}
+    }){
+      const existingUser = await client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id})
+      if(!existingUser){
+        await writeClient.create({
+          _type:'author',
+          id,
+          name,
+          username:login,
+          email,
+          bio:bio || "",
+          image
+
+        })
+
+      }
+      return true;
+    },
+
     async jwt({ token, account , profile }) {
      if(account && profile){
-      token.id = profile?.id
+      const user = await client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id:profile.id})
+      token.id = user._id
       }
       return token;
     },
