5790bb92b43d738092ef35b9dcd7b303b55d08c3
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-05 17:29:54 +0530

    created seperate component file for user project and connected this with the backend

---
+++ components/UserProject.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { client } from '@/sanity/lib/client'
+import { PROJECT_BY_AUTHOR_QUERY } from '@/sanity/lib/queries'
+import { ThreeDCard } from './ThreeDCard';
+import { ProjectTypeCard } from './ProjectCard';
+
+const UserProject = async ({id}:{id:string}) => {
+    const project = await client.fetch(PROJECT_BY_AUTHOR_QUERY,{id})
+    console.log ('project' ,project)
+  return (
+    <>
+     { project?.length > 0 ?(project.map((post:ProjectTypeCard, index:number) =>(
+        <li key={post._id}>
+          <ThreeDCard post = {post} />
+        </li>
+      ))):(<p className="no-result">No projects found.</p>
+      )
+      
+    }
+    </>
+  )
+}
+
+export default UserProject
