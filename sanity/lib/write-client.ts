e9c26ed1e602055ba00bff3abe68913fa4212968
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-05 17:23:47 +0530

    created and setup this file to create a client

---
+++ sanity/lib/write-client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "next-sanity";
+import "server-only"
+import { apiVersion, dataset, projectId,token } from "../env";
+
+export const writeClient = createClient({
+    projectId,
+    dataset,
+    apiVersion,
+    useCdn: false,
+    token,
+});
+if(!writeClient.config().token){
+    throw new Error("Sanity write client token is not found. Please set the SANITY_WRITE_CLIENT environment variable.")
+}
