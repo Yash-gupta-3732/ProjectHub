ac2eac92f32d04f520e39dcb3ca4ec91456a8950
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-05 17:31:11 +0530

    added the format date functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function formatDate(date: Date) {
+
+const daTe = new Date(date);
+  const formatted = new Intl.DateTimeFormat("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(daTe);
+  return formatted;
+
+}
