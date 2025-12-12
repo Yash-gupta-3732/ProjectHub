33f12a6109365341ff317a5278815b37ae30b156
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-12 10:48:46 +0530

    integrated this with backend and also added skeleton loading fallback with the suspense

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import { AnimatedProfileCard } from '@/components/AnimatedProfileCard';
-import { ThreeDCard } from '@/components/ThreeDCard';
+import React, { Suspense } from "react";
+import { AnimatedProfileCard } from "@/components/AnimatedProfileCard";
+import { auth } from "../../../auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import UserProjects from "@/components/UserProject";
+import ProjectListSkeleton from "@/components/ProjectListSkeleton";
 
-const page = ( {params} : {params: {id:string}}) => {
-    const {id} = params;
-    console.log(id);
+
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const session = await auth();
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+  if (!user) return notFound();
+ 
   return (
-    <div>
-       <section className='profile_container'>
-        <AnimatedProfileCard />
-        <div className="flex-1 flex flex-col lg:mt-5">
-          <h1 className='text-3xl font-bold'>All Projects</h1>
-          
-                  <ul className="grid grid-cols-1 md:grid-cols-2 mt-6 justify-center gap-1">
-                    {[
-                      "Project 1",
-                      "Project 2",
-                      "Project 3",
-                      "Project 4",
-                      "Project 5",
-                      "Project 6",
-                    ].map((project, index) => (
-                      <li
-                        key={index}
-                      >
-                       <ThreeDCard />
-                      </li>
-                    ))}
-                  </ul>
-        </div>
-       </section>
-    </div>
-  )
-}
+    <>
+      <div>
+        <section className="profile_container">
+          <AnimatedProfileCard
+            username={user.username}
+            imageUrl={user.image}
+            name={user.name}
+            bio={user.bio}
+          />
+          <div className="flex-1 flex flex-col lg:mt-5">
+            <h1 className="text-3xl font-bold">
+              {session?.id == id ? "Your" : "All"} Projects
+            </h1>
 
-export default page
+            <ul className="grid grid-cols-1 md:grid-cols-2 mt-6 justify-center gap-1">
+              <Suspense fallback={<ProjectListSkeleton length={4}/>}>
+                <UserProjects id={id} />
+              </Suspense>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default page;
