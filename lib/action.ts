30d60705e44aa18e77a21e8ec4c4ced7a4d2d40d
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-12 10:53:58 +0530

    created this file to write the project in the sanity cms

---
+++ lib/action.ts
@@ -0,0 +1,62 @@
+"use server";
+import { auth } from "@/app/auth";
+import { parseServerActionResponse } from "./utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
+
+export const createProject = async <T>(
+  state: T,
+  form: FormData,
+  details: string
+) => {
+  const session = await auth();
+  
+
+  if (!session)
+    return parseServerActionResponse({
+      error: "Unauthorized",
+      status: "error",
+    });
+
+  const { title, description, category, imageurl } = Object.fromEntries(
+    Array.from(form).filter(([key]) => key !== "details")
+  );
+
+  const slug = slugify(title as string, { lower: true, strict: true });
+
+  try {
+    const project = {
+      title,
+      description,
+      category,
+      image: imageurl,
+      details,
+      slug: {
+        _type: "slug",
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session.id,
+      },
+    };
+
+    const result = await writeClient.create({
+      _type: "project",
+      ...project,
+    });
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "success",
+    });
+
+  } catch (error) {
+    console.log(error);
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "error",
+    });
+  }
+};
