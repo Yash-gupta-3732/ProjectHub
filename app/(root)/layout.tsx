d6b79bfc31a17893690492650ed6e6c8e3440e00
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-12 20:39:43 +0530

    added analytics and speed insight functionality into the next app from the vercel

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,8 @@
 import NavBar from "@/components/NavBar";
 import type { ReactNode } from "react";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootGroupLayout({ children }: { children: ReactNode }) {
   // no <html> or <body> here, only in the root layout
@@ -11,6 +13,8 @@ export default function RootGroupLayout({ children }: { children: ReactNode }) {
 
   {children}
   <Toaster richColors position="top-right" />
+  <Analytics/>
+  <SpeedInsights/>
   </main>
   </>;
 }
