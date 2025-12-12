6bf041b81f0fbe7890138dc0823515dd34a611ea
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-12 10:22:48 +0530

    remove the "server-only" to work it only at client side and made some little changes

--- sanity/lib/write-client.ts
+++ sanity/lib/write-client.ts
@@ -1,14 +1,16 @@
+// /sanity/lib/write-client.server.ts
 import { createClient } from "next-sanity";
-import "server-only"
-import { apiVersion, dataset, projectId,token } from "../env";
+import { apiVersion, dataset, projectId, token } from "../env";
 
 export const writeClient = createClient({
-    projectId,
-    dataset,
-    apiVersion,
-    useCdn: false,
-    token,
+  projectId,
+  dataset,
+  apiVersion,
+  token,
+  useCdn: false,
 });
-if(!writeClient.config().token){
-    throw new Error("Sanity write client token is not found. Please set the SANITY_WRITE_CLIENT environment variable.")
+
+// Safety check
+if (!token) {
+  throw new Error("SANITY_WRITE_CLIENT token not found in env");
 }
