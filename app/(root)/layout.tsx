37f0d6c684ce4afe570673a199a64647cb7c151e
Yash-gupta-3732 <147795950+Yash-gupta-3732@users.noreply.github.com>
2025-12-12 10:43:01 +0530

    added toaster

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 // app/(root)/layout.tsx
 import NavBar from "@/components/NavBar";
 import type { ReactNode } from "react";
+import { Toaster } from "sonner";
 
 export default function RootGroupLayout({ children }: { children: ReactNode }) {
   // no <html> or <body> here, only in the root layout
@@ -9,6 +10,7 @@ export default function RootGroupLayout({ children }: { children: ReactNode }) {
     <NavBar />
 
   {children}
+  <Toaster richColors position="top-right" />
   </main>
   </>;
 }
